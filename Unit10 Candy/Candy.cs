4497bb381cd1fa31f4ce914f99cf5760f606e429
DartRuffian <59131299+DartRuffian@users.noreply.github.com>
2023-04-04 19:06:21 -0500

    Unit 10 Lab 02 + Candy

---
+++ Unit10 Candy/Candy.cs
@@ -0,0 +1,31 @@
+﻿internal class Candy
+{
+  public string Name { get; set; }
+  private decimal price;
+
+  public decimal Price
+  {
+    get { return price; }
+    set
+    {
+      if (value >= 0) price = value;
+    }
+  } // end Price
+
+  public Candy(string newName, decimal newPrice)
+  {
+    Name = newName;
+    Price = newPrice;
+  }
+
+  public Candy()
+  {
+    Name = "";
+    Price = 0;
+  }
+
+  public override string ToString()
+  {
+    return $"{Name}, {Price:C}";
+  } //end ToString
+} // end Candy
---
+++ Unit10 Candy/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * @author William Grate
+ * Class Program is a tester class for Class Candy
+ */
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    // Crate a list with a max of MAX_LEN elements
+    const int MAX_LEN = 3;
+    Candy[] bag = new Candy[MAX_LEN];
+
+    FillBag(bag);
+    ShowBagContents(bag);
+  } // end Main
+
+  public static void FillBag(Candy[] bag)
+  {
+    bag[0] = new Candy("Snickers", 1);
+    bag[1] = new Candy("Reece's", 1.25m);
+    bag[2] = new Candy("Rollo's", 0.75m);
+  } // end FillBag
+
+  public static void ShowBagContents(Candy[] bag)
+  {
+    foreach (Candy candy in bag) { Console.WriteLine(candy); }
+  }
+} // end Program
